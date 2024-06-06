1d678c65895e1eae2d9f0d8966098888197cfbbf
Haythem REHOUMA <rehoumahaythem@gmail.com>
2024-06-06 15:11:13 -0400

    Update app_fastapi.py

--- app_fastapi.py
+++ app_fastapi.py
@@ -1,18 +1,14 @@
-# app_fastapi.py
-
 from fastapi import FastAPI
 import numpy as np
 import mlflow.pyfunc
 
 app = FastAPI()
-
-# Charger un modèle MLflow
 model_uri = "models:/Iris_Model/Production"
 model = mlflow.pyfunc.load_model(model_uri)
 
 @app.get("/predict")
 def predict():
-    # Simule la réception d'une entrée de données pour la prédiction
+    # Exemple de prédiction avec des valeurs fixes
     data = np.array([[5.1, 3.5, 1.4, 0.2]])
     prediction = model.predict(data)
-    return {"message": str(prediction)}
+    return {"prediction": prediction.tolist()}  # Convertir en liste pour la compatibilité JSON
